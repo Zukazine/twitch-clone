a65236ea7bc0d7b30e127c0832d3d28176669d1b
Ilyas Irfan <zukazine007@gmail.com>
2024-12-03 16:59:26 +0700

    info card created

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "utfs.io",
+    ],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
